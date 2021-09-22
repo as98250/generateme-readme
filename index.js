1e55cd7193b9d2d9456853c714acaeb16fac3ef0
Ariell SCott <scott.ariell00@gmail.com>
2021-09-22 09:46:13 -0400

    set up databases and updated index

--- index.js
+++ index.js
@@ -1,65 +1,12 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const questions = require('./utils/questions');
 
-// TODO: Create a function to write README file
-function writeToFile(name, description) {
-    console.log(writeToFile);
-}
 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'name'
-    },
-    {
-        type: 'input',
-        message: [(
-            'What was your motivation?',
-            'Why did you build this project?',
-            'What problem does it solve?',
-            'What did you learn?'
-        )],
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: 'Are there any installation instructions?',
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: 'PLease provide an usage instructions?',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: 'Are there any contribution guidlines?',
-        name: 'contribution'
-    },
-    {
-        type: 'input',
-        message: 'How can the user run a test?',
-        name: 'test'
-    },
-    {
-        type: 'checkbox',
-        message: 'What licensing would you like to use?',
-        name: 'license',
-        choices: [
-            'MIT',
-            'GNU CPLv3',
-            'Apache',
-            'Mosilla Public License 2.0'
-        ]
-    }
 
-];
 
 inquirer.prompt(questions).then((answers) => {
-    // here `answers` exist within the scope of the `.then` callback 
     console.log(JSON.stringify(answers));
   });
 
---
+++ utils/questions.js
@@ -0,0 +1,51 @@
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: [(
+            'What was your motivation?',
+            'Why did you build this project?',
+            'What problem does it solve?',
+            'What did you learn?'
+        )],
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Are there any installation instructions?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Please provide an usage instructions?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Are there any contribution guidlines?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'How can the user run a test?',
+        name: 'test'
+    },
+    {
+        type: 'checkbox',
+        message: 'What licensing would you like to use?',
+        name: 'license',
+        choices: [
+            'MIT',
+            'GNU CPLv3',
+            'Apache',
+            'Mosilla Public License 2.0'
+        ]
+    }
+
+];
+
+module.exports = questions;
